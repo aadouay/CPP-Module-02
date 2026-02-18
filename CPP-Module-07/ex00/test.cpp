95d740f18300152101e33deff372146fbf0ceef3
aadouay <synacyb@gmail.com>
2026-02-18 15:11:33 +0000

    final push

--- CPP-Module-07/ex00/test.cpp
+++ CPP-Module-07/ex00/test.cpp
@@ -4,26 +4,85 @@
 
 
 
+// class Animal {
+//     public:
+//         int a = 10;
+//         int b = 22;
+//         virtual ~Animal();
+// };
+
+// class Cat : public Animal{
+//     public:
+//         int j = 0;
+//         int l = 44;
+// };
+
+// class Dog {
+//     public:
+//         std::string name = "mawkli";
+//         int age = 2;
+// };
+
+// int main() {
+
+//     Animal *addr = new Cat; // upcast happing her hytach addr mn type animal not dog !
+//     std::cout << "befor downcast" << std::endl;
+//     std::cout << "a = " << addr->a << std::endl;
+
+//     Dog *down = static_cast<Dog*> (addr);
+//     // std::cout << "after downcast" << std::endl;
+//     std::cout << "a = " << down->age << std::endl;
+
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <string>
+
 class Animal {
-    public:
-        int a = 10;
-        int b = 22;
+public:
+    void help() {std::cout << "waykhan" << std::endl;}
+    virtual ~Animal() {} // Needed for dynamic_cast
 };
 
-class Dog : public Animal {
-    public:
-        int c = 54;
-        int d = 2;
+class Cat :public Animal {
+public:
+    void meow() { std::cout << "Meow!" << std::endl; }
+};
+
+class Dog :public Animal{
+public:
+    std::string secretDogCode = "BONE-123"; // Cat doesn't have this!
+    void bark() { std::cout << "Barking: " << secretDogCode << std::endl; }
 };
 
 int main() {
+    // 1. Create a CAT
+    // Animal* ptr = new Cat();
+    Dog obj;
 
-    Animal *addr = new Dog; // upcast happing her hytach addr mn type animal not dog !
-    std::cout << "befor downcast" << std::endl;
-    std::cout << "a = " << addr->a << std::endl;
+    // 2. STATIC_CAST (The "Dangerous" Way)
+    // This COMPILES because both are Animals.
+    // Dog* badDog = static_cast<Dog*>(ptr); 
+    
+    // std::cout << "--- Static Cast Result ---" << std::endl;
+    // // This will likely CRASH or print garbage because 
+    // // it's trying to read 'secretDogCode' from memory that only contains a Cat.
+    // badDog->bark(); 
 
-    Dog *down = static_cast<Dog*> (addr);
-    std::cout << "after downcast" << std::endl;
-    std::cout << "c = " << down->c << std::endl;
+    // 3. DYNAMIC_CAST (The "Safe" Way)
+    std::cout << "\n--- Dynamic Cast Result ---" << std::endl;
+    Cat* safeCat = dynamic_cast<Cat*>(&obj);
+
+    // if (safeCat == nullptr) {
+    //     std::cout << "Safety Check: This is NOT a dog. Pointer is NULL." << std::endl;
+    // }
+    // safeDog->help();
+    // safeCog->bark();
+    // safeCat->meow();
+    Animal *ikhan = dynamic_cast<Animal *> (safeCat);
+    ikhan->help();
+
     return 0;
 }
--- CPP-Module-07/ex01/test.cpp
+++ CPP-Module-07/ex01/test.cpp
@@ -1,29 +1,46 @@
 #include <iostream>
 #include <cstdint>   // for uintptr_t
 
-struct Data {
-    int value;
+class A{
+    int a;
+    char b;
+    public:
+        A(): a(25), b('a'){}
+        void pranti_ikhan(){
+            std::cout << a << std::endl;
+            std::cout << b << std::endl;
+        }
+};
+
+class C{
+    int a;
+    char b;
+    public:
+        C(): a(65), b('J'){}
+        void pranti_ikhan(){
+            std::cout << a << std::endl;
+            std::cout << b << std::endl;
+        }
+};
+
+class B : public C, A{
+    int a2;
+    char b2;
+    public:
+        B(): a2(15), b2('b'){}
+        void pranti_ikhan(){
+            std::cout << a2 << std::endl;
+            std::cout << b2 << std::endl;
+        }
 };
 
 int main() {
-    Data data;
-    data.value = 42;
 
-    // Original pointer
-    Data* originalPtr = &data;
+    int b = 97;
+    A *i = reinterpret_cast <A *> (&b);
+    i->pranti_ikhan();
+    // char *asscii = reinterpret_cast <char *> (&b);
+    // std::cout << *asscii << std::endl;
 
-    // Pointer → integer (reinterpretation)
-    uintptr_t raw = reinterpret_cast<uintptr_t>(originalPtr);
-
-    // Integer → pointer (reinterpretation)
-    Data* restoredPtr = reinterpret_cast<Data*>(raw);
-
-    // Proof: same address
-    std::cout << "Original pointer : " << originalPtr << std::endl;
-    std::cout << "Restored pointer : " << restoredPtr << std::endl;
-
-    // Proof: same object
-    std::cout << "Value via restored pointer: "
-              << restoredPtr->value << std::endl;
     return 0;
 }
