3f2911455d22c23476da21298dbf71329ce0482f
aadouay <synacyb@gmail.com>
2026-04-07 16:15:52 +0100

    in progress...

--- CPP-Module-09/ex03/main.cpp
+++ CPP-Module-09/ex03/main.cpp
@@ -2,7 +2,7 @@
 #include <stack>
 #include <vector>
 #include <deque>
-
+#include "MutantStack.hpp"
 
 
 /*
@@ -15,35 +15,61 @@
     std::stack<int, std::vector<int>> s2;  // Utilise std::vector à la place
     std::stack<int, std::list<int>> s3;    // Utilise std::list à la place
 */
-class stack_container : public std::stack<int>{
-    public:
-        typedef typename std::stack<int>::container_type::iterator iterator;
-        iterator start(){return this->c.begin();}
-        iterator end(){return this->c.end();}
-};
-int main(){
+// class stack_container : public std::stack<int>{
+//     public:
+//         typedef typename std::stack<int>::container_type::iterator iterator;
+//         iterator start(){return this->c.begin();}
+//         iterator end(){return this->c.end();}
+// };
+// int main(){
 
-    // std::stack <int> stock;
-    // stock.push(24);
-    // stock.push(12);
-    // stock.push(10);
-    // stock.push(23);
-    // stock.push(14);
-    // stock.push(11);
+//     // std::stack <int> stock;
+//     // stock.push(24);
+//     // stock.push(12);
+//     // stock.push(10);
+//     // stock.push(23);
+//     // stock.push(14);
+//     // stock.push(11);
 
-    // while(!stock.empty()){
-    //     std::cout << stock.top() << std::endl;
-    //     stock.pop();
-    // }
+//     // while(!stock.empty()){
+//     //     std::cout << stock.top() << std::endl;
+//     //     stock.pop();
+//     // }
 
-    // std::cout << "size of stock is : " << stock.size() << std::endl;
-    stack_container stock;
-    stock.push(12);
-    stock.push(10);
-    stock.push(14);
-    stock.push(1);
-    stock.push(19);
+//     // std::cout << "size of stock is : " << stock.size() << std::endl;
+//     stack_container stock;
+//     stock.push(12);
+//     stock.push(10);
+//     stock.push(14);
+//     stock.push(1);
+//     stock.push(19);
 
-    for(stack_container::iterator it = stock.start(); it != stock.end(); ++it)
-        std::cout << *it << std::endl;
+//     for(stack_container::iterator it = stock.start(); it != stock.end(); ++it)
+//         std::cout << *it << std::endl;
+// }
+
+int main()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    // std::cout << mstack.top() << std::endl;
+    // mstack.pop();
+    // std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    // ++it;
+    // --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
 }
