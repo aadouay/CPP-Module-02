a472ee9a283b9c8f01ef0dbdecccbeb44db89c27
aadouay <synacyb@gmail.com>
2026-04-09 16:14:07 +0100

    add

--- CPP-Module-09/ex00/test.cpp
+++ CPP-Module-09/ex00/test.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <deque>
+#include <stack>
+
+
 
 int main() {
     std::vector<int> v;
-
+    std::deque <int>(13);
+    std::stack<int, std::vector<int>> a(v);
     std::cout << "Taille : " << v.size() << std::endl;         // Affiche 0
     std::cout << "Capacité : " << v.capacity() << std::endl; // Affiche 0
     if (v.data() == NULL) // .data() renvoie le pointeur vers le bloc interne
