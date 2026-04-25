38c3b979ed402794d9884e8ebb7d49df408c323c
aadouay <synacyb@gmail.com>
2026-04-25 11:59:37 +0100

    wiip

--- CPP-Module-10/ex00/BitcoinExchange.cpp
+++ CPP-Module-10/ex00/BitcoinExchange.cpp
@@ -1,7 +1,7 @@
 #include "BitcoinExchange.hpp"
 
 BitcoinExchange::BitcoinExchange(){}
-BitcoinExchange::BitcoinExchange(const const BitcoinExchange& other):_db(other._db){}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other):_db(other._db){}
 BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other){
     if(this != &other)
         this->_db = other._db;
